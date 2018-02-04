9330b23ba5ec6a4a48bbe2c3311f6a5fcc38d782
ricklacerte <eric.lacerte1@gmail.com>
2018-02-04 12:39:57 -0500

    Add .gitignore (figures) add EX_2.m

---
+++ TP2/Rick/EX2/EX_2.m
@@ -0,0 +1,15 @@
+addpath(genpath('..\MatlabLib'));
+
+%1) On s’intéresse maintenant au canal de Rice. Quelle est la différence entre un canal de
+%  Rayleigh et celui de Rice ?
+
+%2) Pour une fréquence Doppler ?? = 10 ?? ?? ? = 2, générer 100000 échantillons qui
+%   seront transmis sur un canal de Rice à un rythme de 10 ksymbs/s. Vous utiliserez
+%   l’objet « ricianchan » de Matlab.
+
+%3) Sur le même graphique, comparer l’amplitude des échantillons de ce canal à celui de
+%   Rayleigh. Lequel est plus sélectif ? Pourquoi ? Que se passe-t-il si ? = 0 ?
+
+%4) A l’aide d’une simulation Matlab ou Simulink, réaliser des courbes de performances
+%   pour la modulation OQPSK avec ? = [0 2] et une diversité L= [1 2 4]. Commenter
+
